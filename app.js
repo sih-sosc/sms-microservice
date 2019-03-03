b7d94c49425622c2cf52851fe0dbfe6b2642e9af
hm-bilal <hmbilal.998@gmail.com>
2019-03-03 16:35:17 +0530

    Create SMS Bulk service

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser')
+
 
 const keyChecks = require('./middlewares/keyCheck');
 
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', keyChecks, indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,63 @@
 var express = require('express');
 var router = express.Router();
+const way2sms = require('way2sms');
+const handlebar = require('handlebars');
+const nodemailer = require('nodemailer');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.status(200)
-    .send({"message": "Hello World"});
+let smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  auth: {
+      user: req.body.email || process.env.SMTP_EMAIL,
+      pass: req.body.pass || process.env.SMTP_PASSWORD
+  }
 });
 
-router.post('/', function(req, res, next) {
-  
+var mailOptions = {
+  to: recievers[i].email,
+  subject: 'SMS logs',
+  text: text,
+  dsn: {
+      id: recievers[i].id,
+      return: 'headers',
+      notify: ['failure', 'delay'],
+      recipient: req.body.email
+  },
+  html: text
+}
+
+function sendemail(){
+  smtpTransport.sendMail(mailOptions)
+  .then(data => {
+      //  console.log(data)
+  }).catch(err => {
+      // console.log(err)
+      console.log("errorsmtptransport")
+  });
+}
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
   res.status(200)
-    .send({"message": "Successfull Post"})
+    .send({
+      "message": "Hello World"
+    });
+});
+
+let recievers = [];
+
+router.post('/', async function (req, res, next) {
+
+  for (let i = 0; i < recievers.length; i++) {
+    cookie = await way2sms.login(process.env.PHONE, process.env.PASSWORD);
+    await way2sms.send(cookie, recievers[i].number, req.body.text);
+
+    res.status(200)
+      .send({
+        "message": "Successfull Post"
+      })
+  }
+
 });
 
 module.exports = router;
